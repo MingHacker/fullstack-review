f75d1aec2ed27f61daa9232881c4717e761a349f
Moyeong Lee <2000prcs@gmail.com>
2018-04-06 15:40:53 -0700

    Complete getReposByUsername
    - Fetching repo in by sending GET request to Github API

--- helpers/github.js
+++ helpers/github.js
@@ -2,19 +2,34 @@ const request = require('request');
 const config = require('../config.js');
 
 let getReposByUsername = (username) => {
+
+  let userRepos;
+  
+  console.log('username: ', username);
   // TODO - Use the request module to request repos for a specific
   // user from the github API
-  console.log('username: ', username);
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
+
   let options = {
-    url: 'FILL ME IN',
+    url: 'https://api.github.com/users' + '/' + username + '/repos',
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
+  let callback = (error, response, body) => {
+    console.log(response.statusCode);
+    if(!error && response.statusCode === 200){
+      let info = JSON.parse(body);
+      userRepos = info;
+      console.log('info:', info);
+    }
+  };
+
+  request(options, callback);
+  
 }
 
 module.exports.getReposByUsername = getReposByUsername;

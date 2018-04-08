036fe665e0ce4ed7bd5c4b753401fd137c003827
Moyeong Lee <2000prcs@gmail.com>
2018-04-08 11:02:19 -0700

    Change title for the numbers

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -3,7 +3,7 @@ import RepoListUser from './RepoListUser.jsx';
 
 const RepoList = (props) => (
   <div>
-    <h3 id="count"> ### There are {props.userRepos.length} repos in DB total ###</h3>
+    <h3 id="count"> ### {props.userRepos.length} users on the list total ###</h3>
     <h4> TOP 25 REPOS (sorted by recently added)</h4>
         {props.userRepos.map((repo)=>{
             console.log(repo);

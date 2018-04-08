58a53bdfade7bee93abf12a25ff8016792896bc4
Moyeong Lee <2000prcs@gmail.com>
2018-04-08 00:01:01 -0700

    Add title

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,7 +4,7 @@ import RepoListUser from './RepoListUser.jsx';
 const RepoList = (props) => (
   <div>
     <h3 id="count"> ### There are {props.userRepos.length} repos in DB total ###</h3>
-    <h4> TOP 25 REPOS </h4>
+    <h4> TOP 25 REPOS (sorted by recently added)</h4>
         {props.userRepos.map((repo)=>{
             console.log(repo);
             return <RepoListUser key={repo._id} repo={repo}/>
